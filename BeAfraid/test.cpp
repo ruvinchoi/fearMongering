36de04be0803e9578e1f25d07169ceec2485d572
RealPropangandaSpreader <dmclean4311@langleyschools.ca>
2025-06-05 14:40:25 -0700

    felt quirky

--- BeAfraid/test.cpp
+++ BeAfraid/test.cpp
@@ -1,1 +1,25 @@
-pensi
+#include <iostream>
+#include <thread>
+#include <cstdlib>
+#include <mutex>
+
+void run_ollama() {
+    system("ollama run deepseek-r1:8b");
+}
+ 
+void run_custom_command() {
+    std::mutex mtx;
+    mtx.lock();
+    system("/set nothink");
+    mtx.unlock();  
+}
+ 
+int main() {
+    std::thread t1(run_ollama);
+    std::thread t2(run_custom_command);
+ 
+    t1.join();
+    t2.join();
+ 
+    return 0;
+}
